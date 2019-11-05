2edf0e311b88119fc8df48e4b1925abca12dd1ca
Matthew Tsoi <matthew.tsoi@gmail.com>
2019-11-05 14:01:33 +0800

    eda

--- ER_wait_predictor/eda.py
+++ ER_wait_predictor/eda.py
@@ -1,24 +1,26 @@
 import pandas as pd 
 import matplotlib
 
-source_file = './data.csv'
+source_file = '/home/matthew/Desktop/GitHub/AI_Papa/AI_Papa/ER_wait_predictor/data.csv'
 
 ## Load the source csv 
 source_df=pd.read_csv(source_file)
 
 print(source_df.info())
+print(source_df.updateTime) 
 
-##Defines data type 
+# %%Defines data type 
 source_df['topWait']=source_df['topWait'].astype('category')
 source_df['hospName']=source_df['hospName'].astype('category')
-source_df['updateTime']=pd.to_datetime(source_df['updateTime']) 
+source_df['updateTime']=pd.to_datetime(source_df['updateTime'],format='%d/%m/%y %h:%M%p') 
 
-#print(source_df.info())
+print(source_df.info())
 #print(source_df.head(10))
 #print(source_df['topWait'].unique())
+print(source_df.updateTime.max()) 
+print(source_df.updateTime.min()) 
 
-
-##Manual mapping of waiting category  
+# %%Manual mapping of waiting category  
 wait_mapping = [['Around 1 hour', 1], ['Over 1 hour', 2], ['Over 2 hours', 3], ['Over 3 hours', 4], ['Over 4 hours', 5],['Over 5 hours', 6], ['Over 6 hours', 7], ['Over 7 hours', 8], ['Over 8 hours', 9]] 
 wait_mapping_df = pd.DataFrame(wait_mapping, columns = ['Wait Time', 'Wait Time order'],index=None) 
 
@@ -29,3 +31,10 @@ print(source_df.head(5))
 
 plot_df=source_df[source_df['hospName']=='Queen Mary Hospital']
 plot_df.plot(x='updateTime',y='Wait Time order')
+
+
+# %%data exploration
+
+print(source_df.hospName.unique()) 
+print(source_df.updateTime.max()) 
+print(source_df.updateTime.min()) 
