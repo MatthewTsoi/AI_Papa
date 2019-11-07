41cae4a6dd3d733fcd75ff5823e2977b21d60101
Matthew Tsoi <matthew.tsoi@gmail.com>
2019-11-08 02:29:37 +0800

    cleanup

--- ER_wait_predictor/eda.py
+++ ER_wait_predictor/eda.py
@@ -1,24 +1,28 @@
 import pandas as pd 
 import matplotlib
 
-source_file = '/home/matthew/Desktop/GitHub/AI_Papa/AI_Papa/ER_wait_predictor/data.csv'
+def loadData(source_path=''):
 
-## Load the source csv 
-source_df=pd.read_csv(source_file)
+    source_df=pd.read_csv(source_path)
+    print(source_df.info())
+
+    source_df['topWait']=source_df['topWait'].astype('category')
+    source_df['hospName']=source_df['hospName'].astype('category')
+    source_df['updateTime']=pd.to_datetime(source_df['updateTime']) 
+
+    print(source_df['updateTime'].max())
+
+    print(source_df['updateTime'].min())
+    return source_df 
+
+
+# %%Verify data 
+source_df = loadData('data.csv')
+
 
 print(source_df.info())
-print(source_df.updateTime) 
-
-# %%Defines data type 
-source_df['topWait']=source_df['topWait'].astype('category')
-source_df['hospName']=source_df['hospName'].astype('category')
-source_df['updateTime']=pd.to_datetime(source_df['updateTime'],format='%d/%m/%y %h:%M%p') 
-
-print(source_df.info())
-#print(source_df.head(10))
-#print(source_df['topWait'].unique())
-print(source_df.updateTime.max()) 
-print(source_df.updateTime.min()) 
+print('Max wait record:'+str(source_df.updateTime.max()) )
+print('Min wait record:'+str(source_df.updateTime.min()) )
 
 # %%Manual mapping of waiting category  
 wait_mapping = [['Around 1 hour', 1], ['Over 1 hour', 2], ['Over 2 hours', 3], ['Over 3 hours', 4], ['Over 4 hours', 5],['Over 5 hours', 6], ['Over 6 hours', 7], ['Over 7 hours', 8], ['Over 8 hours', 9]] 
@@ -29,12 +33,12 @@ source_df=source_df.drop(columns=['Wait Time'])
 print(source_df.head(5))
 
 
-plot_df=source_df[source_df['hospName']=='Queen Mary Hospital']
-plot_df.plot(x='updateTime',y='Wait Time order')
+#plot_df=source_df[source_df['hospName']=='Queen Mary Hospital']
+#plot_df.plot(x='updateTime',y='Wait Time order')
 
 
 # %%data exploration
 
 print(source_df.hospName.unique()) 
-print(source_df.updateTime.max()) 
-print(source_df.updateTime.min()) 
+#print(source_df.updateTime.max()) 
+#print(source_df.updateTime.min()) 
